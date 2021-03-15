18cbd0a6f6e6d09974f5c89dcc6e5111691d3991
VietNQ <vietcoscc@gmail.com>
2021-03-15 16:13:00 +0700

    first commit

---
+++ src/main/kotlin/com/garithanhtrung/ThanhTrungRestaurantApplication.kt
@@ -0,0 +1,11 @@
+package com.garithanhtrung
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThanhTrungRestaurantApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThanhTrungRestaurantApplication>(*args)
+}
---
+++ src/test/kotlin/com/garithanhtrung/ThanhTrungRestaurantApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.garithanhtrung
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThanhTrungRestaurantApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
